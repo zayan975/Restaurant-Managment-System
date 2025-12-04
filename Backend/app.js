b44551450183e3c237bf3b7ecb4bc975a196d5d5
zayan <zmuhammadzayan237@gmail.com>
2025-12-04 14:46:44 +0500

    export app

--- Backend/app.js
+++ Backend/app.js
@@ -28,6 +28,4 @@ app.use('/api/table', require('./routes/tableRoutes'));
 app.use('/api/payment', require('./routes/paymentRoutes'));
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app
